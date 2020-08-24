62b66ccc7a453be235b096b77ccf1199754cd93b
Moorad <moorad2003@gmail.com>
2020-08-24 04:37:27 +0300

    initial commit, implemented !pkg command and discord server link checking

---
+++ main.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv
+import discord
+import io
+import os
+
+load_dotenv()
+
+prefix = '!'
+rules_channel_id = '637046065140596774';
+
+async def package_cmd(message, args):
+	history = await message.channel.history(limit = 2).flatten()
+
+	file_content = history[1].content
+
+	await message.channel.delete_messages(history)
+	
+	if (file_content.startswith('```') and file_content.endswith('```')):
+		file_content = file_content[file_content.index('\n') + 1:len(file_content) - 1 - file_content[::-1].index('\n')]
+
+	data = io.BytesIO(bytearray(file_content, encoding='utf-8'))
+	filename = 'index.' + args[1]
+	await message.channel.send(file=discord.File(filename=filename, fp=data))
+
+
+class Client(discord.Client):
+	async def on_ready(self):
+		print('Bot running')
+
+	async def on_message(self, message):
+		# filter
+		if ('discord.gg' in message.content):
+			await message.channel.delete_messages([message])
+			await message.channel.send('As stated by rule #1 in <#{channel}>, discord links are not allowed in this server.'
+			.format(channel=rules_channel_id))
+
+		if (not message.content.startswith(prefix)):
+			return
+		
+		args = message.content[1:].split(' ')
+		command = args[0]
+
+		if (command == 'package' or command == 'pkg'):
+			await package_cmd(message, args)
+	
+	async def on_command_error(self, ctx, error):
+		print(error)
+		ctx.message.channel.send('An unexpected error has occured:\n```' + error + '```');
+
+client = Client()
+client.run(os.getenv('DISCORD_TOKEN'))
